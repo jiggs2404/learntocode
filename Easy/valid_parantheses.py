0a8179f18c67197a08ebb816fcaa1d36e52fed81
jiggs2404 <jiggu2404@gmail.com>
2022-01-20 00:38:57 +0530

    Day 8

---
+++ Easy/valid_parantheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+    
+        for char in s:
+            if char in '({[':
+                stack.append(char)
+            elif char == ')':
+                if not stack or stack[-1] != '(':
+                    return False
+                stack.pop()
+            elif char ==  '}':
+                if not stack or stack[-1] != '{':
+                    return False
+                stack.pop()
+            elif char == ']':
+                if not stack or stack[-1] != '[':
+                    return False
+                stack.pop()
+        if not stack:
+            return True
+        return False
